be6428e97855dabf870787d75221a79e79679917
Mantosh <mantosh.pal@pragmaticplay.com>
2023-06-15 11:17:51 +0530

    "second commit"

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function buttonSize(options) {
     let buttonSize = document.querySelectorAll('.buttonSize');
 
     if (options.buttonSize_type === 'large')
-        options.buttonSize_type = '30px';
+        options.buttonSize_type = '200px';
     else if(options.buttonSize_type === 'medium')
-        options.buttonSize_type = '20px';
-    else options.buttonSize_type = '10px';
+        options.buttonSize_type = '150px';
+    else options.buttonSize_type = '50px';
     buttonSize.forEach(buttonSize => {
         buttonSize.style.width = `${options.buttonSize_type}`;
     })
