e27f0087a42b001e7524485460430b7644f0a254
Kolourful23 <136585468+Kolourful23@users.noreply.github.com>
2023-06-15 17:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ function buttonSize(options) {
 
     if (options.buttonSizeLarge_type === 'large')
         options.buttonSizeLarge_type = '200px';
-    else if (options.buttonSizeMed_type === 'medium')
+    if (options.buttonSizeMed_type === 'medium')
         options.buttonSizeMed_type = '150px';
-    else if (options.buttonSizeSmall_type === 'small')
+    if (options.buttonSizeSmall_type === 'smaller')
         options.buttonSizeSmall_type = '50px';
+        
     buttonSizeLarge.forEach(buttonSizeLarge => {
         buttonSizeLarge.style.width = `${options.buttonSizeLarge_type}`;
+        buttonSizeLarge.style.height = `${options.buttonSizeLarge_type}`;
+        buttonSizeLarge.style.fontSize = "24px";
     })
     buttonSizeMed.forEach(buttonSizeMed => {
         buttonSizeMed.style.width = `${options.buttonSizeMed_type}`;
+        buttonSizeMed.style.height = `${options.buttonSizeMed_type}`;
+        buttonSizeMed.style.fontSize = "18px";
     })
     buttonSizeSmall.forEach(buttonSizeSmall => {
         buttonSizeSmall.style.width = `${options.buttonSizeSmall_type}`;
+        buttonSizeSmall.style.height = `${options.buttonSizeSmall_type}`;
+        buttonSizeSmall.style.fontSize = "14px";
     })
 }
 
